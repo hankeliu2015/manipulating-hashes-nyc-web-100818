e1284583086b38804f913055de18402570444d22
Henry <hanke.liu@gmail.com>
2018-10-03 18:59:11 -0400

    passed

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,27 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, data|
+
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+
+        value.each do |element|
+
+          if element == "strawberry"
+              value.delete_if{|x| x = "strawberry"}
+          end
+
+        end
+
+
+      end
+
+    end
+
+  end
 
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,5 @@
+#require 'pry'
+
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -8,6 +10,18 @@ def second_challenge
 
   #code your solution here!
 
-  
+  groceries_list_array = groceries.values
+#binding.pry
+
+  # groceries_list_array.each do |x|
+  #
+  #   groceries_list_array_final.concat(x)
+  #
+  # end
+
+
+
+  groceries_list_array.flatten(1)
+
 
 end
